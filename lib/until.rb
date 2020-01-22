c45e9c158bd2220c6d1efc106d57e6738b9a939d
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-22 19:23:46 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
   levitation_force = 6
-  until using_until_levitaion_force == 6
+  until using_untillevitaion_force == 6
       puts "this number is less than 6"
-      using_until_levitaion_force += 1
+      using_untillevitaion_force += 1
     end
 end
 

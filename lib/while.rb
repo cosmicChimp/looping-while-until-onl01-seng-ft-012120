f5b22589b28bd6af647afc3a0c612263390f30a8
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-22 19:23:20 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 6
-	if counter >= 6
+	if using_whilelevitation_force >= 6
 	  puts "This number is greater or equal to 6"
-	  counter += 1
+	  using_whilelevitation_force += 1
 	  break
   
 end
